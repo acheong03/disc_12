c4181fa6ccf8d8cc8c64d190fd7e629640b26f3f
acheong03 <122408422+acheong03@users.noreply.github.com>
2023-03-29 19:56:01 -0400

    Add files via upload

--- discussion12.py
+++ discussion12.py
@@ -39,12 +39,16 @@ def create_species_table(cur, conn):
 # TASK 1
 # CREATE TABLE FOR PATIENTS IN DATABASE
 def create_patients_table(cur, conn):
-    pass
+    cur.execute("DROP TABLE IF EXISTS Patients")
+    cur.execute("CREATE TABLE Patients (id INTEGER PRIMARY KEY, name TEXT, species_id INTEGER, aggressiveness INTEGER)")
+    conn.commit()
+    
 
 
 # ADD FLUFFLE TO THE TABLE
 def add_fluffle(cur, conn):
-    pass
+    cur.execute("INSERT INTO Patients (name, species_id, aggressiveness) VALUES ('Fluffle', 0, 2)")
+    conn.commit()
     
 
 # TASK 2
@@ -59,13 +63,16 @@ def add_pets_from_json(filename, cur, conn):
     json_data = json.loads(file_data)
 
     # THE REST IS UP TO YOU
-    pass
+    for pet in json_data['pets']:
+        cur.execute("INSERT INTO Patients (name, species_id, aggressiveness) VALUES (?, ?, ?)", (pet['name'], pet['species_id'], pet['aggressiveness']))
+    conn.commit()
 
 
 # TASK 3
 # CODE TO OUTPUT NON-AGGRESSIVE PETS
 def non_aggressive_pets(aggressiveness, cur, conn):
-    pass
+    cur.execute("SELECT Patients.name, Species.title FROM Patients JOIN Species ON Patients.species_id = Species.id WHERE aggressiveness < ?", (aggressiveness,))
+    return cur.fetchall()
 
 
 
